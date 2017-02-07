19984cad1da495d48824f428ce760c19406a041c
Manuel Leßmann <lessmann.manuel@gmail.com>
2017-02-07 22:24:08 +0100

    Fixed .resx file namespace

--- Transformations/Transformations/ErrorStrings.Designer.cs
+++ Transformations/Transformations/ErrorStrings.Designer.cs
@@ -40,7 +40,7 @@ namespace NMF.Transformations {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Transformations.ErrorStrings", typeof(ErrorStrings).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NMF.Transformations.ErrorStrings", typeof(ErrorStrings).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
